d36e7c663ae6861ea7d0819e415c7d9d7ad89250
Vitor Finotti <vfinotti@ncc.unesp.br>
2019-03-05 19:25:05 -0300

    Adding startup configuration on main and leaving while empty

--- main.c
+++ main.c
@@ -104,64 +104,22 @@ void Interrupt0_Handler(void)
 
 int main(void)
 {
-  unsigned int counter; // dummy
-  unsigned int ii;      // loop iterator
-  unsigned int trap;    // memory access pattern receiver
-  unsigned int period;  // time interval for memory access
-  period=1000000;      // period for FPGA implementation; roughly 3 seconds for a 10MHz osc in CM0_DS
-  /* period=100;           // period for simulations in ARM/Keil MDK and Xilinx ISIM tool */
+  period=500000U;       // period for FPGA implementation; blinking some times for a 10MHz osc in CM0_DS
+  /* period=300U;          // period for simulations in Xilinx Vivado tool */
 
-  signed long int x,y,z;
+  // Setting Timer interrupt
+  TIMER_MTIMECMP0 = period;
+  TIMER_MTIME = 0U;
+  TIMER_IRQ_EN = 1U;
+  TIMER_GLO_PRESCALE = 0U;
+
+  __enable_irq();
+
+  NVIC_SetPriority (0, 1);            // Set Timer priority
+  NVIC_EnableIRQ (0);                 // Enable Timer Interrupt
 
   while(1)
     {
 
-      // Calculate sine and cosine of pi/3
-      CONTROL_START0 = 0;
-      X0 = 1073741824; // 1 in Q1.30 notation
-      Y0 = 0;
-      Z0 = 715827883; // angle pi/3, since 2pi = 2^32
-      CONTROL_START0 = 1;
-
-      // Calculate sine and cosine of pi/4
-      CONTROL_START1 = 0;
-      X1 = 1073741824; // 1 in Q1.30 notation
-      Y1 = 0;
-      Z1 = 536870912; // angle pi/4, since 2pi = 2^32
-      CONTROL_START1 = 1;
-
-      // Calculate sine and cosine of pi/6
-      CONTROL_START2 = 0;
-      X2 = 1073741824; // 1 in Q1.30 notation
-      Y2 = 0;
-      Z2 = 357913941; // angle pi/6, since 2pi = 2^32
-      CONTROL_START2 = 1;
-
-      // Calculate sine and cosine of pi
-      CONTROL_START3 = 0;
-      X3 = 1073741824; // 1 in Q1.30 notation
-      Y3 = 0;
-      Z3 = 4294967295; // angle pi, since 2pi = 2^32
-      CONTROL_START3 = 1;
-
-      // Wait for calculations results
-      while(CONTROL_DONE0 != 1)
-      {}
-      while(CONTROL_DONE1 != 1)
-      {}
-      while(CONTROL_DONE2 != 1)
-      {}
-      while(CONTROL_DONE3 != 1)
-      {}
-
-
-      // count until period and put the pattern that toggles the LED on the bus
-      counter=0;
-      for(ii=0; ii<period; ii++)
-        {
-          counter++;
-        }
-      trap=LedToggle; // memory access pattern (toggle LED)
-      trap++; // force toggle value to change value
     }
 }
