66c1b849d87942df111a9a44e845761b43ee5705
Vitor Finotti <vfinotti@ncc.unesp.br>
2019-03-05 19:24:28 -0300

    Adding interruption function

--- main.c
+++ main.c
@@ -64,6 +64,42 @@ volatile unsigned int trap;    // memory access pattern receiver
 volatile unsigned int period;  // time interval for memory access
 
 
+void Interrupt0_Handler(void)
+{
+  // Calculate sine and cosine of pi/3
+  CONTROL_START0 = 0;
+  X0 = 1073741824; // 1 in Q1.30 notation
+  Y0 = 0;
+  Z0 = 715827883; // angle pi/3, since 2pi = 2^32
+  CONTROL_START0 = 1;
+
+  // Calculate sine and cosine of pi/4
+  CONTROL_START1 = 0;
+  X1 = 1073741824; // 1 in Q1.30 notation
+  Y1 = 0;
+  Z1 = 536870912; // angle pi/4, since 2pi = 2^32
+  CONTROL_START1 = 1;
+
+  // Calculate sine and cosine of pi/6
+  CONTROL_START2 = 0;
+  X2 = 1073741824; // 1 in Q1.30 notation
+  Y2 = 0;
+  Z2 = 357913941; // angle pi/6, since 2pi = 2^32
+  CONTROL_START2 = 1;
+
+  // Calculate sine and cosine of pi
+  CONTROL_START3 = 0;
+  X3 = 1073741824; // 1 in Q1.30 notation
+  Y3 = 0;
+  Z3 = 4294967295; // angle pi, since 2pi = 2^32
+  CONTROL_START3 = 1;
+
+  trap=LedToggle; // memory access pattern (toggle LED)
+
+  // Reset timer
+  TIMER_MTIME = 0U;
+
+}
 
 
 int main(void)
